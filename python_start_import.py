d7bdf15bcd664ff20dc0cebf3d573119847dd999
Mark Blackmore <mblackmo@yahoo.com>
2018-04-22 12:04:05 -0700

    Initial commit

---
+++ python_start_import.py
@@ -0,0 +1,25 @@
+# Installing Python Packages
+
+# Get Package Import Manager "pip":
+# http://pip.readthedocs.io/en/stable/installing/#installing-with-get-pip-py
+
+# Upgrade pip
+# python -m pip install -U pip
+
+# Install some packages
+# pip3 install numpy
+# pip3 install pandas
+
+# First NumPy Array
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(np_baseball)
+
